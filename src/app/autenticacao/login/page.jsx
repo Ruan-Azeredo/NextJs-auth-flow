b52e09cf02a0c9d7691aa2baa661472e7ad8746a
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-04-28 13:11:31 -0300

    :truck: chore: passa login page para /autenticacao

--- src/app/autenticacao/login/page.jsx
+++ src/app/autenticacao/login/page.jsx
@@ -1,16 +1,28 @@
-import Image from 'next/image'
+'use client'
+
+import Alert from "@/components/Alert"
+import { AuthContext } from "@/contexts/Auth"
+import { useContext, useState } from "react"
 
 export default function Login() {
+
+  const {signIn, error} = useContext(AuthContext)
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value)
+  }
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value)
+  }
+
+  console.log(email, password)
+
   return (
     <>
-      {/*
-        This example requires updating your template:
-
-        ```
-        <html class="h-full bg-white">
-        <body class="h-full">
-        ```
-      */}
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
@@ -24,7 +36,8 @@ export default function Login() {
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
+          <Alert error={error} />
+          <div className="space-y-6" action="#" method="POST">
             <div>
               <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                 Email address
@@ -36,6 +49,7 @@ export default function Login() {
                   type="email"
                   autoComplete="email"
                   required
+                  onChange={handleEmail}
                   className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:outline-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -59,6 +73,7 @@ export default function Login() {
                   type="password"
                   autoComplete="current-password"
                   required
+                  onChange={handlePassword}
                   className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:outline-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -66,18 +81,19 @@ export default function Login() {
 
             <div>
               <button
-                type="submit"
+                onClick={() => signIn(email, password)}
+                
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Sign in
               </button>
             </div>
-          </form>
+          </div>
 
           <p className="mt-10 text-center text-sm text-gray-500">
             Not a member?{' '}
             <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-              Start a 14 day free trial
+              Register here
             </a>
           </p>
         </div>
