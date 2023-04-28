aab80d7cd9852ee97e052d67c98ced916cafa8ed
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-04-28 15:55:07 -0300

    :sparkles: feat: simula fluxo de cadastro

---
+++ src/app/autenticacao/cadastro/page.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Cadastro = () => {
+
+    // Fluxo para cadastro
+    
+    // Primerio faz uma chamada API do tipo POST para cadastrar o usuario
+    
+    // Em seguida, segue-se o fluxo de autenticação de login com:
+    
+    // const { signIn } = useContext(AuthContext)
+
+    // signIn()
+
+    return (
+        <div>Cadastro</div>
+    )
+}
+
+export default Cadastro
